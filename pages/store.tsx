578a7ac679dc09df8c165c13f32482dd5e7c42c3
Jack Allen <jackallen@bulb.co.uk>
2022-09-16 12:51:11 +0100

    tabs

--- pages/store.tsx
+++ pages/store.tsx
@@ -1,27 +1,16 @@
 import type { NextPage } from "next";
-import { useForm } from "react-hook-form";
+import { Formik, Form, Field } from "formik";
 import { useEffect } from "react";
 import { useRouter } from "next/router";
 import { Query } from "../components/store/Query";
 import { Tabs } from "../components/store/Tabs";
+import { BoltIcon } from "@heroicons/react/24/outline";
 
 function Tab() {
   const { push, query } = useRouter();
 
   const projectId = query.projectId as string;
   const path = query.path as string;
-
-  const { register, handleSubmit, setValue } = useForm<{
-    path: string;
-    projectId: string;
-  }>({
-    defaultValues: query,
-  });
-
-  useEffect(() => {
-    setValue("projectId", projectId);
-    setValue("path", path);
-  }, [projectId, path]);
 
   async function onSubmit(values: { path: string; projectId: string }) {
     await push({
@@ -35,24 +24,49 @@ function Tab() {
   return (
     <div className="flex flex-col flex-1 overflow-hidden">
       <div className="p-5">
-        <form onSubmit={handleSubmit(onSubmit)} className="flex">
-          <div className="flex-1 flex flex-row bg-white/5 border-white/20 border rounded-md divide-x divide-white/20 overflow-hidden">
-            <input
-              {...register("projectId")}
-              placeholder="Enter a Project Id"
-              className="placeholder:text-xs bg-transparent font-mono text-sm px-4 h-10 outline-none autofill:!bg-red-500 appearance-none"
-            />
-            <input
-              {...register("path")}
-              placeholder="Enter a collection/document path"
-              className="placeholder:text-xs bg-transparent font-mono text-sm px-4 h-10 outline-none flex-1 autofill:bg-white/20"
-            />
-          </div>
-          <button className="bg-sky-600 rounded-md w-32 font-normal text-sm ml-2">
-            Send
-          </button>
-          <input type="submit" hidden={true} />
-        </form>
+        <Formik
+          onSubmit={onSubmit}
+          enableReinitialize
+          initialValues={{
+            path: query.path as string ?? '',
+            projectId: query.projectId as string ?? '',
+          }}
+        >
+          <Form className="grid gap-2">
+            <div className="flex">
+              <div className="flex-1 flex flex-row bg-white/5 border-white/20 border rounded-md divide-x divide-white/20 overflow-hidden">
+                <Field
+                  name="projectId"
+                  placeholder="Enter a Project Id"
+                  className="placeholder:text-xs bg-transparent flex-1 sm:flex-grow-0 font-mono text-sm px-4 h-10 outline-none autofill:!bg-red-500 appearance-none"
+                />
+
+                <Field
+                  name="path"
+                  placeholder="Enter a collection/document path"
+                  className="placeholder:text-xs bg-transparent hidden sm:block font-mono text-sm px-4 h-10 outline-none flex-1 autofill:bg-white/20"
+                />
+              </div>
+              <button
+                type="submit"
+                className="bg-sky-600 rounded-md flex justify-center items-center px-3 sm:w-32 font-normal text-sm ml-2"
+              >
+                <div className="hidden sm:block">Send</div>
+                <div className="sm:hidden block">
+                  <BoltIcon className="w-5 h-5" />
+                </div>
+              </button>
+            </div>
+
+            <div className="flex-1 flex flex-row bg-white/5 border-white/20 border rounded-md divide-x divide-white/20 overflow-hidden  sm:hidden block">
+              <Field
+                name="path"
+                placeholder="Enter a collection/document path"
+                className="placeholder:text-xs bg-transparent font-mono text-sm px-4 h-10 outline-none flex-1 autofill:bg-white/20"
+              />
+            </div>
+          </Form>
+        </Formik>
       </div>
 
       <div className="overflow-auto flex flex-1 pb-5 px-5">
