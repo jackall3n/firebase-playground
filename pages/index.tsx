7530f86aa506f093b6dfba0a971ea963f8483656
Jack Allen <jackallen@bulb.co.uk>
2022-09-16 11:07:03 +0100

    home

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,25 +1,47 @@
 import type {NextPage} from 'next'
-import {useForm} from 'react-hook-form';
-
-function Tab() {
-    const {register, getValues} = useForm<{ query: string }>();
-
-    return (
-        <form>
-            <input {...register('query')} />
-            {JSON.stringify(getValues())}
-        </form>
-    )
-}
+import Link from 'next/link';
 
 const Home: NextPage = () => {
     return (
-        <div>
+        <div className="flex flex-col justify-center items-center flex-1">
             <div>
-                Tabs
-            </div>
+                <div className="pb-10">
+                    <div className="font-medium font-mono">
+                        <div className="text-3xl font-extrabold text-transparent bg-clip-text bg-gradient-to-br from-red-700 to-yellow-400">Firebase
+                        </div>
+                        <span className="text-sm"> Playground</span>
+                    </div>
+                </div>
 
-            <Tab/>
+                <div className="grid grid-cols-2 aspect-square gap-4">
+                    <Link href="/store">
+                        <a
+                            className="rounded-md border border-white/10 aspect-square p-5 text-center flex flex-col justify-center items-center bg-white/5">
+                            <div className="">🔥</div>
+                            <div className="font-semibold tracking-wide my-1">Firestore</div>
+                            <div className="text-sm text-gray-500">Inspect your Firestore instance</div>
+                        </a>
+                    </Link>
+                    <div
+                        className="rounded-md border border-white/10 aspect-square p-5 text-center flex flex-col justify-center items-center bg-white/5">
+                        <div className="">💿</div>
+                        <div className="font-semibold tracking-wide my-1">Database</div>
+                        <div className="text-sm text-gray-500">Coming soon 🚀</div>
+                    </div>
+                    <div
+                        className="rounded-md border border-white/10 aspect-square p-5text-center flex flex-col justify-center items-center bg-white/5">
+                        <div className="">🤖</div>
+                        <div className="font-semibold tracking-wide my-1">Functions</div>
+                        <div className="text-sm text-gray-500">Coming soon 🚀</div>
+                    </div>
+                    <div
+                        className="rounded-md border border-white/10 aspect-square p-5text-center flex flex-col justify-center items-center bg-white/5">
+                        <div className="">👮🏽</div>
+                        <div className="font-semibold tracking-wide my-1">Auth</div>
+                        <div className="text-sm text-gray-500">Coming soon 🚀</div>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
